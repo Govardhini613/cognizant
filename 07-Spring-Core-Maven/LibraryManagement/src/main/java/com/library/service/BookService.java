541e8f26dd786f55d3d49a1facf13dd92ddfdfec
govardhinipolamarasetty <govardhiipolamaraetty@gmail.com>
2026-07-12 19:26:46 +0530

    Completed Spring Core Exercise 7 - Constructor and Setter Injection

--- 07-Spring-Core-Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ 07-Spring-Core-Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -1,14 +1,21 @@
 package com.library.service;
 
 import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookService {
 
     private BookRepository bookRepository;
 
-    public BookService() {
+    // Constructor Injection
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
 
+    // Setter Injection
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
