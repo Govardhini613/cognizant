e8ad8ffdc120886ff808df6bb05ac97ea4e81b16
govardhinipolamarasetty <govardhiipolamaraetty@gmail.com>
2026-07-18 22:05:28 +0530

    Completed Spring Data JPA Hands-on 5 - Country Service CRUD

--- 08-Spring-Data-JPA/OrmLearn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
+++ 08-Spring-Data-JPA/OrmLearn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import com.cognizant.ormlearn.model.Country;
 import com.cognizant.ormlearn.service.CountryService;
 
 @SpringBootApplication
@@ -16,8 +16,14 @@ public class OrmLearnApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(OrmLearnApplication.class, args);
     }
-   	 @Override
-      public void run(String... args) throws Exception {
-        System.out.println(countryService.getCountry("IN"));
-    }
+   	  @Override
+public void run(String... args) throws Exception {
+
+    Country country = new Country("JP", "Japan Updated");
+
+    countryService.updateCountry(country);
+
+    System.out.println(countryService.getCountry("JP"));
+
+}
 }
