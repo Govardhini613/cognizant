8ba3ac45adf38aa23558ae663418a6659b387a8b
govardhinipolamarasetty <govardhiipolamaraetty@gmail.com>
2026-07-26 16:59:24 +0530

    Completed React Hands-on 7 - Routing

--- 11-React/react-handson/src/App.js
+++ 11-React/react-handson/src/App.js
@@ -1,10 +1,24 @@
-import ButtonClick from "./components/ButtonClick";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div>
-      <ButtonClick />
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/about">About</Link>
+        </nav>
+
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
